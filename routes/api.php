5b9d9a77d18063347f0ff4800cdc88e8cda4db04
abdelra7man <abdelra7manabdullah@gmail.com>
2024-12-01 19:11:22 +0200

    FIX: Update api Endpoints URL

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,21 @@ use App\Http\Controllers\API\v1\{
 Route::prefix('users')
     ->as('users.')
     ->group(function () {
-        Route::post('register', UserRegisterController::class)->name('register');
-        Route::get('verify-email', [VerifyEmailController::class,'verifyEmail'])->name('verify-email');
-        Route::post('login', [UserSessionController::class,'login'])->name('login');
+        Route::post('/register', UserRegisterController::class)->name('register');
+        Route::get('/verify-email', [VerifyEmailController::class,'verifyEmail'])->name('verify-email');
+        Route::post('/login', [UserSessionController::class,'login'])->name('login');
 
         Route::post('/login/google',[GoogleLoginController::class,'redirectToGoogle'])->name('login.google');
 
         Route::middleware('auth:sanctum')->group(function () {
-            Route::post('logout', [UserSessionController::class,'logout'])->name('logout');
+            Route::post('/logout', [UserSessionController::class,'logout'])->name('logout');
+
+            Route::controller(ProfileController::class)
+                ->group(function () {
+                    Route::get('/profile','show')->name('profile.show');
+                    Route::put('/profile/update','update')->name('profile.update');
+                    Route::delete('/profile/delete-account','destroy')->name('profile.destroy');
+                });
         });
 
     });
